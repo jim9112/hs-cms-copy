172a5de7b28ca09edaf8f16d96f4908704311a18
James Hannan <jim9112@gmail.com>
2024-11-11 19:55:10 -0500

    add in explanation section on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,3 +14,16 @@
     </div>
   </div>
 </div>
+<div class="container mx-auto py-10">
+  <div class="max-w-md mx-auto">
+    <h2 class="text-4xl font-bold text-center mb-7">How to Use</h2>
+    <p>
+      Each tool will use a <a
+        href="https://developers.hubspot.com/beta-docs/guides/apps/private-apps/overview"
+        target="_blank">private app key</a
+      >. You will need to be a super admin in order to use a lot of the features
+      in this tool. The different tools will tell you what scopes are needed in
+      order to use it.
+    </p>
+  </div>
+</div>
